da7cef8c94245544800887a1e0f2f2fb40af1651
BananaHedgehog <izzyrudge2@gmail.com>
2025-01-22 13:13:38 +0000

    linked drone collection to win

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public PlayerController plrController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +23,7 @@ public class Collectible : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             //win
-            Debug.Log("win");
+            plrController.Win();
         }
     }
 }
