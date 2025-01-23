95298b3cb25b04e251c3bd9c2766ec7d3a0bcd73
Amber <amber@transcental.dev>
2025-01-23 00:29:05 +0000

    how to play and build stuff

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,11 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private UIDocument learnModal;
+
+    private void Awake()
+    {
+        var returnButton = learnModal.rootVisualElement.Q<Button>("return");
+        returnButton.clicked += HideModal;
+        HideModal();
+    }
+    
     public void OnPlayButton() {
         SceneManager.LoadScene(1);
+    }
+
+    public void OnLearnButton()
+    {
+        learnModal.rootVisualElement.style.display = DisplayStyle.Flex;
+    }
+
+    public void HideModal()
+    {
+        learnModal.rootVisualElement.style.display = DisplayStyle.None;
     }
     
     public void OnQuitButton() {
