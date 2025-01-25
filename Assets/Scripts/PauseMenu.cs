fe262f26cccf767b7c8e64f8f78b16f711e3318e
Amber <amber@transcental.dev>
2025-01-25 20:43:12 +0000

    add learn to pause i think

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,6 +5,7 @@ using Cursor = UnityEngine.Cursor;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private UIDocument pauseMenu;
+    [SerializeField] private UIDocument learnModal;
     private bool isPaused;
 
     public bool GetIsPaused()
@@ -14,17 +15,24 @@ public class PauseMenu : MonoBehaviour
 
     private void Awake()
     {
+        pauseMenu.rootVisualElement.style.display = DisplayStyle.None;
+        learnModal.rootVisualElement.style.display = DisplayStyle.None;
+
         var resumeButton = pauseMenu.rootVisualElement.Q<Button>("resume");
         var restartButton = pauseMenu.rootVisualElement.Q<Button>("restart");
+        var learnButton = pauseMenu.rootVisualElement.Q<Button>("learn");
         var quitButton = pauseMenu.rootVisualElement.Q<Button>("quit");
-        
+
         resumeButton.clicked += Resume;
         restartButton.clicked += Restart;
+        learnButton.clicked += ShowHelp;
         quitButton.clicked += Quit;
 
         Time.timeScale = 1;
         isPaused = false;
-        pauseMenu.rootVisualElement.style.display = DisplayStyle.None;
+
+        var returnButton = learnModal.rootVisualElement.Q<Button>("return");
+        returnButton.clicked += () => learnModal.rootVisualElement.style.display = DisplayStyle.None;
     }
     
     void Update()
@@ -63,6 +71,12 @@ public class PauseMenu : MonoBehaviour
     
     private void Quit() {
         Application.Quit();
+    }
+
+    private void ShowHelp()
+    {
+        pauseMenu.rootVisualElement.style.display = DisplayStyle.None;
+        learnModal.rootVisualElement.style.display = DisplayStyle.Flex;
     }
     
     private void Restart() {
